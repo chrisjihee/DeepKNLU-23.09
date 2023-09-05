4c88a227072d7d2e118d031abea2620162b62b3d
Jihee Ryu <chrisjihee@etri.re.kr>
2023-09-05 19:47:51 +0900

    Add requirements.txt Update setup-env.sh

--- setup-env.sh
+++ setup-env.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-conda create -n DeepKNLU-23.07 python=3.10 -y
-conda activate DeepKNLU-23.07
+conda create -n DeepKNLU-23.09 python=3.10 -y
+conda activate DeepKNLU-23.09
 pip install -r requirements.txt
 pip list
 
+# optional for editing library
 rm -rf chrisbase chrislab
 git clone git@github.com:chrisjihee/chrisbase.git
 git clone git@github.com:chrisjihee/chrislab.git
 pip install --editable chrisbase
 pip install --editable chrislab
-
-rm -f pretrained-com pretrained-pro
-ln -s ../pretrained-com .
-ln -s ../pretrained-pro .
