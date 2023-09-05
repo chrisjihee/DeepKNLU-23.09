4cc2956a939a13f7ea7a694e02b479ed50fdc05e
Jihee Ryu <chrisjihee@etri.re.kr>
2023-09-06 07:41:42 +0900

    Add serve()

--- 1-doc-class.py
+++ 1-doc-class.py
@@ -4,14 +4,16 @@ from typing import List
 
 import torch
 import typer
+from flask import Flask
 from pytorch_lightning import Trainer
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 from transformers import AutoTokenizer, AutoConfig, AutoModelForSequenceClassification
+from transformers.modeling_outputs import SequenceClassifierOutput
 
 import nlpbook
 from chrisbase.data import AppTyper, JobTimer, ArgumentsUsing, RuntimeChecking
 from chrisbase.io import hr
-from nlpbook.arguments import TrainerArguments, TesterArguments
+from nlpbook.arguments import TrainerArguments, TesterArguments, ServerArguments
 from nlpbook.cls import NsmcCorpus, ClassificationDataset, ClassificationTask
 
 logger = logging.getLogger(__name__)
@@ -206,6 +208,103 @@ def test(
                                                epoch_steps=len(test_dataloader)),
                             dataloaders=test_dataloader,
                             ckpt_path=checkpoint_path)
+
+
+@app.command()
+def serve(
+        # env
+        project: str = typer.Option(default="DeepKNLU"),
+        job_name: str = typer.Option(default=None),
+        debugging: bool = typer.Option(default=False),
+        # data
+        data_name: str = typer.Option(default="nsmc"),
+        train_file: str = typer.Option(default=None),
+        valid_file: str = typer.Option(default=None),
+        test_file: str = typer.Option(default="ratings_test.txt"),
+        num_check: int = typer.Option(default=2),
+        # model
+        pretrained: str = typer.Option(default="pretrained/KPF-BERT"),
+        model_name: str = typer.Option(default=None),
+        seq_len: int = typer.Option(default=64),
+        # hardware
+        accelerator: str = typer.Option(default="gpu"),
+        precision: str = typer.Option(default="16-mixed"),
+        strategy: str = typer.Option(default="auto"),
+        device: List[int] = typer.Option(default=[0]),
+        batch_size: int = typer.Option(default=64),
+):
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    torch.set_float32_matmul_precision('high')
+    logging.getLogger("fsspec.local").setLevel(logging.WARNING)
+    logging.getLogger("transformers.configuration_utils").setLevel(logging.WARNING)
+    args = ServerArguments.from_args(
+        project=project,
+        job_name=job_name,
+        debugging=debugging,
+        data_name=data_name,
+        train_file=train_file,
+        valid_file=valid_file,
+        test_file=test_file,
+        num_check=num_check,
+        pretrained=pretrained,
+        model_name=model_name,
+        seq_len=seq_len,
+        accelerator=accelerator,
+        precision=precision,
+        strategy=strategy,
+        device=device,
+        batch_size=batch_size,
+    )
+    with JobTimer(f"python {args.env.running_file} {' '.join(args.env.command_args)}", rt=1, rb=1, rc='=', verbose=True, flush_sec=0.3):
+        with ArgumentsUsing(args):
+            args.info_args()
+            corpus = NsmcCorpus(args)
+            tokenizer = AutoTokenizer.from_pretrained(args.model.pretrained, use_fast=True)
+            checkpoint_path = args.env.output_home / args.model.name
+            assert checkpoint_path.exists(), f"No checkpoint file: {checkpoint_path}"
+            logger.info(f"Using finetuned checkpoint file at {checkpoint_path}")
+            checkpoint: dict = torch.load(checkpoint_path, map_location=torch.device("cpu"))
+            logger.info(hr('-'))
+
+            pretrained_model_config = AutoConfig.from_pretrained(
+                args.model.pretrained,
+                num_labels=corpus.num_labels
+            )
+            model = AutoModelForSequenceClassification.from_pretrained(
+                args.model.pretrained,
+                config=pretrained_model_config,
+            )
+            model.load_state_dict({k.replace("model.", ""): v for k, v in checkpoint['state_dict'].items()})
+            model.eval()
+            logger.info(hr('-'))
+
+            def inference_fn(sentence):
+                inputs = tokenizer(
+                    [sentence],
+                    max_length=args.model.seq_len,
+                    padding="max_length",
+                    truncation=True,
+                )
+                with torch.no_grad():
+                    outputs: SequenceClassifierOutput = model(**{k: torch.tensor(v) for k, v in inputs.items()})
+                    prob = outputs.logits.softmax(dim=1)
+                    positive_prob = round(prob[0][1].item(), 4)
+                    negative_prob = round(prob[0][0].item(), 4)
+                    pred = "긍정 (positive)" if torch.argmax(prob) == 1 else "부정 (negative)"
+                return {
+                    'sentence': sentence,
+                    'prediction': pred,
+                    'positive_data': f"긍정 {positive_prob:.4f}",
+                    'negative_data': f"부정 {negative_prob:.4f}",
+                    'positive_width': f"{positive_prob * 100}%",
+                    'negative_width': f"{negative_prob * 100}%",
+                }
+
+            with RuntimeChecking(args.configure_csv_logger()):
+                server: Flask = nlpbook.make_server(inference_fn,
+                                                    template_file="serve_cls.html",
+                                                    ngrok_home=args.env.working_path)
+                server.run()
 
 
 if __name__ == "__main__":
